41aa56b82a8a7a14cf8e77483eab28fee61e6dc0
Kingbegaa <barakachachabegaa@gmail.com>
2024-08-28 22:32:33 +0300

    Update reaction.js

--- scs/reaction.js
+++ scs/reaction.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { zokou } = require("../framework/zokou");
+const { adams } = require("../Ibrahim/adams");
 const fs = require("fs-extra");
 const { exec } = require("child_process");
 const child_process = require('child_process');
@@ -28,7 +28,7 @@ const GIFBufferToVideoBuffer = async (image) => {
 };
 
 const generateReactionCommand = (reactionName, reactionEmoji) => {
-    zokou({
+    adams({
         nomCom: reactionName,
         categorie: "Reaction",
         reaction: reactionEmoji,
