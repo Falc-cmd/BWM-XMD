03e20a8e0e7d64d2b88fbe640bd2afd4c7f07bbf
ekitale <munjueko@gmail.com>
2024-08-29 09:07:36 +0300

    Hii nisha edit mkuu

--- scs/bmwz.js
+++ scs/bmwz.js
@@ -1,6 +1,6 @@
-const { adams } = require('../Ibrahim/adams);
+const { adams } = require('../Ibrahim/adams');
 
-adams({ nomCom: 'quote', categorie: 'general' }, async (dest, zk, commandeOptions) => {
+adams({ nomCom: 'quote', categorie: 'Fun' }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, verifGroupe, arg } = commandeOptions;
   if (!verifGroupe) {
     repondre('Commande réservée au groupe uniquement');
