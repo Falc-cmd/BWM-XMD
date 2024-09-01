1a2039dd1deed7bbe07ce64c9aae219ab365a120
ibraah tech <munjueko@gmail.com>
2024-09-01 22:04:38 +0300

    Update bmwtes.js

--- scs/bmwtes.js
+++ scs/bmwtes.js
@@ -155,7 +155,7 @@ adams({
         // Prepare the message with song details
         const infoMess = {
           image: { url: videos[0].thumbnail },
-          caption: `*BMW SONG PLAYER*\n\n*©Ibrahim Adams*`
+          caption: `BMW SONG PLAYER*\n\n*◁ II ▷ 1:00 •၊၊||၊|။||။||၊|||။ ၊|•*${videos[0].timestamp}`
         };
 
         // Send song details
@@ -214,7 +214,7 @@ adams({
         // Prepare the message with song details
         const infoMess = {
           image: { url: videos[0].thumbnail },
-          caption: `*BMW SONG PLAYER*\n\n*©Ibrahim Adams*`
+          caption: `BMW SONG PLAYER*\n\n*◁ II ▷ 1:00 •၊၊||၊|။||။||၊|||။ ၊|•*${videos[0].timestamp}`
         };
 
         // Send song details
