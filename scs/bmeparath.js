8767ead77b27d3cc783fbde0e97ee68e4b015744
ibraah tech <munjueko@gmail.com>
2024-09-01 17:11:40 +0300

    Create bmeparath.js

---
+++ scs/bmeparath.js
@@ -0,0 +1,56 @@
+const { adams } = require("../Ibrahim/adams");
+const yts = require('yt-search');
+
+adams({
+  nomCom: "sing",
+  categorie: "Search",
+  reaction: "🎧"
+}, async (origineMessage, zk, commandeOptions) => {
+  const { ms, repondre, arg } = commandeOptions;
+     
+  if (!arg[0]) {
+    repondre("Please provide song name.");
+    return;
+  }
+
+  try {
+    let topo = arg.join(" ");
+    const search = await yts(topo);
+    const videos = search.videos;
+
+    if (videos && videos.length > 0 && videos[0]) {
+      const urlElement = videos[0].url;
+      const apiResponse = await fetch(`https://api.prabath-md.tech/api/ytmp3?url=${encodeURIComponent(urlElement)}`);
+      const apiResult = await apiResponse.json();
+
+      if (apiResult.status === 'success ✅') {
+        const audioUrl = apiResult.data.download;
+        const fileInfo = {
+          title: apiResult.data.title,
+          fileSize: apiResult.data.file_size,
+          quality: apiResult.data.quality
+        };
+
+        let infoMess = {
+          image: { url: videos[0].thumbnail },
+          caption: `*BMW SONG PLAYER*`
+        };
+
+        zk.sendMessage(origineMessage, infoMess, { quoted: ms });
+
+        // Send the audio file using the audio URL
+        zk.sendMessage(origineMessage, { audio: { url: audioUrl }, mimetype: 'audio/mp4' }, { quoted: ms, ptt: false });
+        console.log("Sending audio file completed!");
+
+        await ms.React('✅');
+        repondre('Download Success...');
+      } else {
+        repondre('Failed to download audio. Please try again later.');
+      }
+    } else {
+      repondre('No videos found.');
+    }
+  } catch (error) {
+    console.error('Error from API:', error);
+  }
+});
